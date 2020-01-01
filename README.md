8a5d36e6c42f6504578f819a7239af1a8d8394a2
Stefan Radu <stefan270101@gmail.com>
2020-01-01 21:31:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Quotes
+
+First project using Rust. The exectuable takes as an argument the path to a quote file and prints out a random quote from that file.
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate rand;
+
+use::std::env;
+use::std::process;
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+use rand::Rng;
+
+fn main() -> io::Result < () > {
+
+    let args: Vec < String > = env::args().collect();
+
+    if args.len() == 1 {
+        eprintln!("No file name passed");
+        process::exit(-1);
+    }
+
+    if args.len() > 2 {
+        eprintln!("Too many arguments!");
+    }
+
+    let f = File::open(args[1].clone())?;
+    let f = BufReader::new(f);
+
+    let mut quotes: Vec < String > = Vec::new();
+
+    for line in f.lines() {
+        let s = line.unwrap();
+        if s.len() > 10 {
+            quotes.push(s[2..].to_string());
+        }
+    }
+
+    let mut rng = rand::thread_rng();
+    println!("{}\n", quotes[rng.gen_range(0, quotes.len())]);
+
+    Ok(())
+}
