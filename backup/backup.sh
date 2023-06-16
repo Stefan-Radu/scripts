8cb42279db70541b7b6bf004b3cb53ce2d8afe64
Stefan Radu <stefan270101@gmail.com>
2023-06-17 01:35:12 +0300

    use whoami

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,8 +2,8 @@
 
 if [[ -d "/media/backup" ]]; then
     echo "Drive mounted. Running backup..."
-    rsync -av --delete --progress --log-file=/home/stef/Desktop/backup.log --exclude="Movies" --exclude=".vbox" --exclude=".cache" /home/stef /media/backup/
-    chown stef:wheel /home/stef/Desktop/backup.log
+    rsync -av --delete --progress --log-file=/home/$(whoami)/Desktop/backup.log --exclude="Movies" --exclude=".vbox" --exclude=".cache" /home/$(whoami) /media/backup/
+    chown $(whoami):wheel /home/$(whoami)/Desktop/backup.log
 else
     echo "Drive not mounted."
 fi
